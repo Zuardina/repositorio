7ad03cb6341f9eb533262de22edd9a7596ade283
SA\20085527 <almir.procopio@br.yazaki.com>
2023-01-18 21:27:45 -0300

    master - primeira subida

---
+++ src/ApiAso.Model/Pessoa/PessoaContatoModel.cs
@@ -0,0 +1,14 @@
+﻿using ApiAso.Model.Base;
+
+namespace ApiAso.Model.Pessoa
+{
+    public class PessoaContatoModel : BaseModel
+    {
+        public int idPessoaContato { get; set; }
+        public int idPessoa { get; set; }
+        public int idAppTipoContato { get; set; }
+        public string Valor { get; set; }
+        public string Contato { get; set; }
+        public string Observacao { get; set; }
+    }
+}
