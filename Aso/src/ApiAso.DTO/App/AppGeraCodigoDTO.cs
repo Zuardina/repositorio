230eebfb1842634aa5b190f6877de44fcef8e31e
Lara <83315878+GoddessPersephone@users.noreply.github.com>
2023-02-18 15:18:31 -0300

    Subindo alterações de repositorio novo

---
+++ Aso/src/ApiAso.DTO/App/AppGeraCodigoDTO.cs
@@ -0,0 +1,12 @@
+﻿using ApiAso.DTO.Base;
+
+namespace ApiAso.DTO.App
+{
+    public class AppGeraCodigoDTO : BaseDTO
+    {
+        public int idAppGeraCodigo { get; set; }
+        public int idEmpresa { get; set; }
+        public string Proprietario { get; set; }
+    
+    }
+}
---
+++ Aso/src/ApiAso.Repositorio/App/AppGeraCodigoRepositorio.cs
@@ -0,0 +1,79 @@
+﻿using ApiAso.DTO.App;
+using ApiAso.Model.App;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text;
+
+namespace ApiAso.Repositorio.App
+{
+    public class AppGeraCodigoRepositorio : IDisposable
+    {
+        private bool flagDispose;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (flagDispose)
+                return;
+
+            if (disposing)
+            {
+            }
+
+            flagDispose = true;
+        }
+
+        #region App Gera Codigo
+        public List<AppGeraCodigoModel> ObterAppGeraCodigo(AppGeraCodigoDTO filtro)
+        {
+            var sentencaSql = ObterSentencaSqlAppGeraCodigo();
+            if (filtro.idAppGeraCodigo > 0)
+                sentencaSql.Append($" AND idAppGeraCodigo = {filtro.idAppGeraCodigo}");
+            else if
+               (filtro.idEmpresa > 0)
+               sentencaSql.Append($" AND idEmpresa = {filtro.idEmpresa}");
+            else
+            return null;
+            switch (filtro.PesquisaPor)
+            {
+                case Enumerador.ePesquisaPor.PorId:
+               
+                    break;
+                case Enumerador.ePesquisaPor.PorDescricaoIgualdade:
+                    if (!string.IsNullOrEmpty(filtro.Proprietario))
+                        sentencaSql.Append($" AND Proprietario = '{filtro.Proprietario}'");
+
+                    break;
+                case Enumerador.ePesquisaPor.PorDescricaoPorComparacao:
+                    if (!string.IsNullOrEmpty(filtro.Proprietario))
+                        sentencaSql.Append($" AND Proprietario LIKE '%{filtro.Proprietario}%'");
+
+                    break;
+            }
+
+            //Log.Infor($"Execução da sentença {sentencaSql.ToString()}")
+            using (var conexao = new MySqlConnection(""))
+            {
+                return conexao.Query<AppGeraCodigoModel>(sentencaSql.ToString(), commandType: CommandType.Text).ToList();
+            }
+        }
+
+        private StringBuilder ObterSentencaSqlAppGeraCodigo()
+        {
+            return new StringBuilder($@"SELECT idAppGeraCodigo
+                                              ,idEmpresa
+                                              ,Proprietario
+                                              ,Sequencia
+                                          FROM AppGeraCodigo
+                                         WHERE FlagExcluido = 0
+                                         "); // AND idEmpresa = 57"
+        }
+        #endregion
+
+    }
+}
